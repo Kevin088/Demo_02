ea36b89b81d1d12179d148bc3215c8dc13d53526
xululu <284904817@qq.com>
2016-11-01 14:04:40 +0800

    提交

---
+++ listvideoplayer/src/main/java/com/demo/cn/mytest/MyTextureView.java
@@ -0,0 +1,25 @@
+package com.demo.cn.mytest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+/**
+ * Created by xll on 2016/11/1.
+ */
+
+public class MyTextureView extends TextureView {
+    public MyTextureView(Context context) {
+        super(context);
+    }
+
+    public MyTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
