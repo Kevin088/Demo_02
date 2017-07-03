04fc96c0e157fcfe574fee9661a2b51a86069476
xull <xullxull@dfhe.com>
2017-07-03 11:19:26 +0800

    1.

---
+++ sharesdktest/src/main/java/cn/dfhe/supoff/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.dfhe.supoff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.sharesdktest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
