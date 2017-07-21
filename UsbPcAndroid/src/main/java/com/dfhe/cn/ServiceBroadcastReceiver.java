b5b56868245fd12cef4c4870cfb84fbb2bd95159
xull <xullxull@dfhe.com>
2017-07-21 14:04:13 +0800

    1.

---
+++ UsbPcAndroid/src/main/java/com/dfhe/cn/ServiceBroadcastReceiver.java
@@ -0,0 +1,30 @@
+package com.dfhe.cn;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ServiceBroadcastReceiver extends BroadcastReceiver {
+	private static String START_ACTION = "NotifyServiceStart";
+	private static String STOP_ACTION = "NotifyServiceStop";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.e(AndroidService_.TAG, Thread.currentThread().getName() + "---->"
+				+ "ServiceBroadcastReceiver onReceive");
+
+		String action = intent.getAction();
+		if (START_ACTION.equalsIgnoreCase(action)) {
+			context.startService(new Intent(context, AndroidService_.class));
+
+			Log.d(AndroidService_.TAG, Thread.currentThread().getName() + "---->"
+					+ "ServiceBroadcastReceiver onReceive start end");
+		} else if (STOP_ACTION.equalsIgnoreCase(action)) {
+			context.stopService(new Intent(context, AndroidService_.class));
+			Log.d(AndroidService_.TAG, Thread.currentThread().getName() + "---->"
+					+ "ServiceBroadcastReceiver onReceive stop end");
+		}
+	}
+
+}
